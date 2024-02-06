39d7a0f7a4fcfb37525bee5ae57f702a2b144c96
James Kulu <james.kulu@outcodesoftware.com>
2024-02-06 17:22:37 +0545

    removed animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,30 +54,30 @@ const sr = ScrollReveal({
 });
 
 /*SCROLL HOME*/
-sr.reveal('.home__title', {});
-sr.reveal('.home__scroll', { delay: 200 });
-sr.reveal('.home__img', { origin: 'right', delay: 400 });
+// sr.reveal('.home__title', {});
+// sr.reveal('.home__scroll', { delay: 200 });
+// sr.reveal('.home__img', { origin: 'right', delay: 400 });
 
-/*SCROLL ABOUT*/
-sr.reveal('.about__img', { delay: 500 });
-sr.reveal('.about__subtitle', { delay: 300 });
-sr.reveal('.about__profession', { delay: 400 });
-sr.reveal('.about__text', { delay: 500 });
-sr.reveal('.about__social-icon', { delay: 600, interval: 200 });
+// /*SCROLL ABOUT*/
+// sr.reveal('.about__img', { delay: 500 });
+// sr.reveal('.about__subtitle', { delay: 300 });
+// sr.reveal('.about__profession', { delay: 400 });
+// sr.reveal('.about__text', { delay: 500 });
+// sr.reveal('.about__social-icon', { delay: 600, interval: 200 });
 
-/*SCROLL SKILLS*/
-sr.reveal('.skills__subtitle', {});
-sr.reveal('.skills__name', { distance: '20px', delay: 50, interval: 100 });
-sr.reveal('.skills__img', { delay: 400 });
+// /*SCROLL SKILLS*/
+// sr.reveal('.skills__subtitle', {});
+// sr.reveal('.skills__name', { distance: '20px', delay: 50, interval: 100 });
+// sr.reveal('.skills__img', { delay: 400 });
 
-/*SCROLL PORTFOLIO*/
-sr.reveal('.portfolio__img', { interval: 200 });
+// /*SCROLL PORTFOLIO*/
+// sr.reveal('.portfolio__img', { interval: 200 });
 
-/*SCROLL CONTACT*/
-sr.reveal('.contact__subtitle', {});
-sr.reveal('.contact__text', { interval: 200 });
-sr.reveal('.contact__input', { delay: 400 });
-sr.reveal('.contact__button', { delay: 600 });
+// /*SCROLL CONTACT*/
+// sr.reveal('.contact__subtitle', {});
+// sr.reveal('.contact__text', { interval: 200 });
+// sr.reveal('.contact__input', { delay: 400 });
+// sr.reveal('.contact__button', { delay: 600 });
 
 const contactForm = document.querySelector('#contact-form');
 contactForm.addEventListener('submit', submitForm);
